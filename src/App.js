6665911399cba8623fa9ded97042876634a458fa
LSB <tmfql805@naver.com>
2022-09-16 18:12:23 +0900

    영화앱 만들기 완전 기초!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { func } from "prop-types";
+import React, { useState } from "react";
+import { findDOMNode } from "react-dom";
+// import Movies from "./components/Movies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// state는 동적데이터를 다룰 때 사용하는 리액트 요소
+
+// function App() {
+//   return <div>함수 컴포넌트</div>;
+// }
+
+// class App extends React.Component {
+//   // 클래스 컴포넌트(react.component상속)
+//   render() {
+//     return <div>클래스 컴포넌트</div>;
+//   }
+// }
+
+// function App(props) {
+//   // 함수컴포넌트에서 state변수 쓰는 법2
+//   const [count] = useState(0);
+// }
+
+class App extends React.Component {
+  test = 1; // 멤버변수
+  // App 클래스 생성자
+  constructor(props) {
+    super(props); //부모 생성자 호출
+    this.state = {
+      count: 0,
+    };
+  }
+
+  증가함수 = () => {
+    console.log("증가함수");
+    // 방법1
+    // this.setState({ count: this.state.count + 1 }); //reder를 바뀌는 부분만 다시 호출해줌 // 값 설정하는 방법
+    // 방법2
+    this.setState(function (현재state) {
+      return { count: 현재state.count + 1 };
+    });
+  };
+  감소함수 = () => {
+    console.log("감소함수");
+    // 방법1;
+    // this.setState({ count: this.state.count - 1 });
+    // 방법2
+    this.setState(function (현재state) {
+      return { count: 현재state.count - 1 };
+    });
+  };
+
+  render() {
+    console.log("랜더함수 호출됨");
+    console.log(this.state);
+    return (
+      <div>
+        <h1>카운터 : {this.state.count}</h1>
+        <button onClick={this.증가함수}>+1</button>{" "}
+        <button onClick={this.감소함수}>-1</button>
+      </div>
+    );
+  }
 }
 
 export default App;
